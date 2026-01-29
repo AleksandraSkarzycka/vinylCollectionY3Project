360bc5ffb818cd3ba09bb6838400fa6f0986666e
Aleksandra Skarzycka <c00294397@setu.ie>
2026-01-29 12:59:36 +0000

    Added in 3 vinyl pictues and titles

--- vinylCollection/src/routes/+page.svelte
+++ vinylCollection/src/routes/+page.svelte
@@ -2,21 +2,31 @@
     import { base } from '$app/paths';
 </script>
 
+<div class="title">
+    <p>Aleksandra's Vinyls</p>
+</div>
+
 <div class="vinylContainer">
     <div class="vinyl1">
         <div class="vImage">
-
+            <img src="{base}/img/sunburnDF.jpg" alt="Cover picture for Dominic Fike's Sunburn album" width="95%">
         </div>
+
+        <p class="vTitle">Dominic Fike - Sunburn</p>
     </div>
     <div class="vinyl2">
         <div class="vImage">
-            
+            <img src="{base}/img/14minutesDF.jpg" alt="Cover picture for Dominic Fike's 14 Minutes album" width="95%">
         </div>
+        
+        <p class="vTitle">Dominic Fike - 14 Minutes</p>
     </div>
     <div class="vinyl3">
         <div class="vImage">
-            
+            <img src="{base}/img/wcpgwDF.jpg" alt="Cover picture for Dominic Fike's What Could Possibly Go Wrong album" width="95%">
         </div>
+
+        <p class="vTitle">Dominic Fike - What Could Possibly Go Wrong</p>
     </div>
     <div class="vinyl4">
         <div class="vImage">
@@ -73,7 +83,6 @@
         background-color: rgba(241, 231, 191);
         max-width: 70%;
         max-height: fit-content;
-        margin-top: 60px;
     }
     .vinyl1, .vinyl2, .vinyl3, .vinyl4, .vinyl5,
     .vinyl6, .vinyl7, .vinyl8, .vinyl9, .vinyl10,
@@ -85,6 +94,9 @@
         min-height: 350px;
         margin: 1rem;
         border-radius: 2%;
+
+        display: grid;
+        place-items: center;
     }
 
     .vImage {
@@ -93,7 +105,22 @@
         min-width: 70%;
         max-height: 100%;
         min-height: 70%;
-        margin: 1rem;
+        margin: 0.5rem;
         border-radius: 2%;
+        padding-top: 2%;
+    }
+    .title {
+        font-family: 'font3', sans-serif;
+        padding-top: 85px;
+        font-size: xxx-large;
+    }
+
+    img {
+        border-radius: 2%;
+    }
+
+    .vTitle {
+        font-family: 'font3', sans-serif;
+        font-size: 15px;
     }
 </style>
