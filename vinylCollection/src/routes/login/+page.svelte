43657686d9d4320ea9efc2f1e59be3348b204a8e
Aleksandra Skarzycka <c00294397@setu.ie>
2026-04-24 01:55:32 +0100

    Added files, updated CSS for login page

--- vinylCollection/src/routes/login/+page.svelte
+++ vinylCollection/src/routes/login/+page.svelte
@@ -20,12 +20,12 @@
 </script>
 
 <svelte:head>
-	<title>User Management</title>
+	<title>Login</title>
 </svelte:head>
 
 <form class="row flex flex-center" method="POST" use:enhance={handleSubmit}>
 	<div class="col-6 form-widget">
-		<h1 class="header">Supabase + SvelteKit</h1>
+		<h1 class="header">Login via Email</h1>
 		<p class="description">Sign in via magic link with your email below</p>
 		{#if form?.message !== undefined}
 		<div class="success {form?.success ? '' : 'fail'}">
@@ -55,3 +55,55 @@
 		</div>
 	</div>
 </form>
+
+<style>
+
+	.flex-center {
+		background-color: #fdf5e6;
+		height: 100vh;
+		max-width: 100%;
+		align-content: center;
+		justify-items: center;
+	}
+	.form-widget {
+		max-width: 50%;
+		width: 50%;
+		height: 50vh;
+		background-color: #e6dfd1;
+		border-radius: 20px;
+		justify-content: center;
+		align-content: center;
+	}
+	.inputField {
+		width: 40%;
+		padding: 0.5rem;
+		margin-top: 0.5rem;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+	}
+	.button {
+		margin-top: 1rem;
+		padding: 0.75rem;
+		border: none;
+		border-radius: 4px;
+		color: black;
+		background-color: #cccccc;
+		cursor: pointer;
+	}
+	.button.primary {
+		background-color: #cccccc;
+	}
+	.success {
+		width: 60%;
+		margin-top: 1rem;
+		padding: 0.75rem;
+		border-radius: 4px;
+		color: white;
+		background-color: #28a745;
+		justify-self: center;
+	}
+	.success.fail {
+		background-color: #dc3545;
+	}
+
+</style>
